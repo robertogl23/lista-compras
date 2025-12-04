f788e4bd8f9ec8b3f7b070a14584adea1ff228be
Roberto Carlos Galicia Lopez <49224574+robertogl23@users.noreply.github.com>
2025-12-04 17:12:51 -0600

    se agrega service-worker

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "lista-compras-v1";
+
+const URLS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest"
+  // agrega íconos aquí cuando los tengas, ej:
+  // "./icons/icon-192.png",
+  // "./icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
+});
